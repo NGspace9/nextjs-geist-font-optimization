4c144c83759c40c23a6ca2e08904eecee8741040
NGspace9 <ngspaceorganizacao@gmail.com>
2025-07-09 13:08:55 -0300

    Add .next copy/static/chunks/src_app_page_tsx_9148afbf._.js 1752077345503

---
+++ .next copy/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7ce62792._.js",
+  "static/chunks/src_c5b74a55._.js"
+],
+    source: "dynamic"
+});
